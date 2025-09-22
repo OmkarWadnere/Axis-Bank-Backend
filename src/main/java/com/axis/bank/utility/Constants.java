1b27f82d6d3a6e3ba6b417ab3ba5550791ada57e
OmkarWadnere <omkar.wadnere0779@gmail.com>
2025-09-22 17:47:42 +0530

    (Axis-Bank-Clone) : fixed logs traceId and operationId.

--- src/main/java/com/axis/bank/utility/Constants.java
+++ src/main/java/com/axis/bank/utility/Constants.java
@@ -7,4 +7,6 @@ public class Constants {
 
     public static final String OPERATION_ID = "operationId";
     public static final String TRACE_ID = "traceId";
+    public static final String X_TRACE_ID = "X-traceId";
+    public static final String X_OPERATION_ID = "X-operationId";
 }
